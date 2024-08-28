600254e76a6621fed970a0c4cc8663c1dfe7a990
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-28 16:59:44 -0400

    agrego cambios

--- app/src/main/java/com/example/boton/HomeActivity.kt
+++ app/src/main/java/com/example/boton/HomeActivity.kt
@@ -6,18 +6,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
-
     private lateinit var btnHome: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
         btnHome = findViewById(R.id.btn_home)
-
         btnHome.setOnClickListener {
             Toast.makeText(this, "¡Has hecho clic en el botón!", Toast.LENGTH_SHORT).show()
         }
-
     }
 }
